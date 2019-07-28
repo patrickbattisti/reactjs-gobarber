7d600cfe49bc5128bcd52f53fca58aca52c541b6
Patrick Battisti <1729patrick@gmail.com>
2019-07-28 20:17:24 -0300

    Atualzação dos dados do usuário

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
+import { updateProfileRequest } from '~/store/modules/user/actions';
 import { Container } from './styles';
 
 const Profile = () => {
+  const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
-  const handleSubmit = data => {};
+  const handleSubmit = data => {
+    dispatch(updateProfileRequest(data));
+  };
 
   return (
     <Container>
