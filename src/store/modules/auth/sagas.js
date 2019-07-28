f73a1672cca89de62d8ad50b40cfc6cea46bb1de
Patrick Battisti <1729patrick@gmail.com>
2019-07-28 15:31:57 -0300

    Mensagem de erro

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import api from '~/services/api';
 import history from '~/services/history';
@@ -16,6 +17,8 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
+      toast.error('Usuário não é prestador.');
+      yield put(signFailure());
       return;
     }
 
@@ -23,6 +26,7 @@ export function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (e) {
+    toast.error('Falha na autenticação, verifique seus dados.');
     yield put(signFailure());
   }
 }
