fcc1363a81b35250a87c219fcf2271f0e78111fd
Patrick Battisti <1729patrick@gmail.com>
2019-07-28 22:04:53 -0300

    Estilização do dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 const Dashboard = () => {
-  return <h1>dash</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft color="#fff" size={36} />
+        </button>
+        <strong>hoje</strong>
+        <button type="button">
+          <MdChevronRight color="#fff" size={36} />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Patrick Battisti</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em Aberto</span>
+        </Time>
+        <Time>
+          <strong>08:00</strong>
+          <span>Patrick Battisti</span>
+        </Time>
+        <Time>
+          <strong>08:00</strong>
+          <span>Patrick Battisti</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 };
 
 export default Dashboard;
